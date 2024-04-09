5418cfa2566ec46371c5df23ecadd0eb6cc1be5a
RichardKoudou <richard.koudou@epsi.fr>
2024-04-09 20:18:36 +0200

    implementation de la page de creation de compte

---
+++ app/(tabs)/register.tsx
@@ -0,0 +1,36 @@
+import React, {useState} from "react";
+import {View, Text, TextInput, Button, Alert } from "react-native";
+import {registerUser} from './apiRequests';
+
+const RegisterPage: React.FC = () => {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleRegister = () => {
+        registerUser(email, password);
+    };
+
+    return (
+        <View>
+
+            <Text>
+                Email : 
+            </Text>
+            <TextInput
+                onChangeText={(text) => setEmail(text)}
+                value={email}
+                placeholder="Entrez votre adresse email (exemple@exemple.com)" />
+            <Text>Mot de passe: </Text>
+            <TextInput 
+                onChangeText={(text) => setPassword(text)}
+                value={password}
+                placeholder="Entrez votre mot de passe" 
+                secureTextEntry 
+            />
+            <Button title="Créer un compte" onPress={handleRegister} />
+    </View>
+    );
+};
+
+export default RegisterPage;
