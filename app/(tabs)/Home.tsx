8b6f43a232e0134bbda1efeee5df4974a7ac3ccf
emmarft <emma.ruffet@epsi.fr>
2024-04-10 11:49:11 +0200

    barre de recherche

--- app/(tabs)/Home.tsx
+++ app/(tabs)/Home.tsx
@@ -1,8 +1,6 @@
-import { StyleSheet, Image } from 'react-native';
+import { StyleSheet, TextInput, Text, View, Image, ActivityIndicator, FlatList } from 'react-native';
 import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
 import React, { useEffect, useState } from 'react';
-import { ActivityIndicator, FlatList } from 'react-native';
 
 type Data = {
   id: string;
@@ -21,6 +19,7 @@ type Data = {
 export default function TabOneScreenApp() {
   const [isLoading, setLoading] = useState(true);
   const [data, setData] = useState<Data[]>([]);
+  const [searchQuery, setSearchQuery] = useState('');
 
   const getData = async () => {
     try {
@@ -38,33 +37,62 @@ export default function TabOneScreenApp() {
     getData();
   }, []);
 
+  const handleSearch = (text: string) => {
+    setSearchQuery(text);
+  };
+
+  const renderHighlightedText = (text: string) => {
+    if (!searchQuery) {
+      return <Text>{text}</Text>;
+    }
+  
+    const regex = new RegExp(`\\b${searchQuery}\\b`, 'gi');
+    const parts = text.split(regex);
+  
+    return parts.map((part, index) => (
+      regex.test(part) ? <Text key={index} style={styles.highlight}>{part}</Text> : <Text key={index}>{part}</Text>
+    ));
+  };  
+
+  const filteredData = data.filter(item =>
+    item.name.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
   return (
     <View style={styles.container}>
+      <View style={styles.searchContainer}>
+        <TextInput
+          style={styles.searchInput}
+          placeholder="Recherche..."
+          value={searchQuery}
+          onChangeText={handleSearch}
+        />
+      </View>
       {isLoading ? (
         <ActivityIndicator />
       ) : (
         <FlatList
-          data={data}
+          data={filteredData}
           keyExtractor={({ id }) => id}
           renderItem={({ item }) => (
             <View key={item.id} style={styles.item}>
               <Text style={styles.label}>Nom: </Text>
-              <Text style={styles.value}>{item.name}</Text>
+              {renderHighlightedText(item.name)}
               
               <Text style={styles.label}>Description: </Text>
-              <Text style={styles.value}>{item.description}</Text>
+              {renderHighlightedText(item.description)}
               
               <Text style={styles.label}>Créé à: </Text>
-              <Text style={styles.value}>{item.created_at}</Text>
+              {renderHighlightedText(item.created_at)}
               
               <Text style={styles.label}>Image: </Text>
               <Image source={{uri: item.path_image}} style={styles.image}/>
               
               <Text style={styles.label}>Publié à: </Text>
-              <Text style={styles.value}>{item.updated_at}</Text>
+              {renderHighlightedText(item.updated_at)}
               
               <Text style={styles.label}>Posté par: </Text>
-              <Text style={styles.value}>{item.user_created_name}</Text>
+              {renderHighlightedText(item.user_created_name)}
             </View>
           )}
         />
@@ -76,28 +104,44 @@ export default function TabOneScreenApp() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: 'white',
+  },
+  searchContainer: {
     alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor: 'white',
+  },
+  searchInput: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    borderRadius: 8,
+    paddingHorizontal: 10,
+    marginTop: 20,
+    marginBottom: 10,
+    width: '93%',
   },
   item: {
     marginBottom: 20,
-    backgroundColor: 'gray',
+    backgroundColor: '#eee',
     borderRadius: 20,
     padding: 10,
+    marginHorizontal: 10,
   },
   label: {
     fontWeight: 'bold',
     marginBottom: 5,
-    color: 'black', 
+    color: 'black',
   },
   value: {
     marginBottom: 10,
-    color: 'black', 
+    color: 'black',
   },
   image: {
-    width: 400, 
-    height: 400, 
+    width: 380, 
+    height: 380, 
     marginBottom: 10,
+  },
+  highlight: {
+    backgroundColor: 'pink',
   },
 });
